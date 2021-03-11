bae5c051498bef09c6a67a40e0fa6f743b97dea9
mattkyea <kyeamatt@gmail.com>
2021-03-11 12:57:19 -0500

    I have a basic sorting working - will add dynamic input and clean

--- Android/app/src/main/java/com/example/assign4_list/MainActivity.java
+++ Android/app/src/main/java/com/example/assign4_list/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+        Button commitText = (Button) findViewById(R.id.button);
         String[] words = new String[100];
         for(int i=0; i<100; i++){
             words[i] = "words" + i;
         }
-        RecycleViewAdapter a = new RecycleViewAdapter(words);
-        recyclerView.setAdapter(a);
+        final RecycleViewAdapter[] a = {new RecycleViewAdapter(words)};
+        recyclerView.setAdapter(a[0]);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        commitText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                words[99] = "clicked";
+                Arrays.sort(words);
+                a[0] = new RecycleViewAdapter(words);
+                recyclerView.setAdapter(a[0]);
+            }
+        });
     }
 
 }
