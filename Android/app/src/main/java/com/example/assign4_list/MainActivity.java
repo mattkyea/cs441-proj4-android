7df40e2cb25b7512eb7d8fdcd696bb8b9ca0d202
mattkyea <kyeamatt@gmail.com>
2021-03-11 21:26:46 -0500

    added comments, probably last commit

--- Android/app/src/main/java/com/example/assign4_list/MainActivity.java
+++ Android/app/src/main/java/com/example/assign4_list/MainActivity.java
@@ -18,15 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("created");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //3 xml elements
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
         Button commitTextButton = (Button) findViewById(R.id.button);
         TextView  textToCommit = (TextView) findViewById(R.id.textEntry);
 
+        //this will store all elements in the list
         ArrayList<String> words = new ArrayList<>();
 
+        //populate with some default values
         words.add("dog");
         words.add("cat");
         words.add("horse");
@@ -35,20 +38,21 @@ public class MainActivity extends AppCompatActivity {
         words.add("giraffe");
         words.add("lion");
 
+        //custom comparator to sort in alphabetical order
         words.sort((s1, s2) -> s1.compareTo(s2));
 
-        final RecycleViewAdapter[] a = {new RecycleViewAdapter(words, recyclerView)};
-        recyclerView.setAdapter(a[0]);
+        //recycle view setup
+        final RecycleViewAdapter[] a = {new RecycleViewAdapter(words, recyclerView)};//call my adapter with the arraylist and view
+        //need list for elements, view for click handler
+        recyclerView.setAdapter(a[0]);//set view and my adapter
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        commitTextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                words.add(textToCommit.getText().toString());
-                words.sort((s1, s2) -> s1.compareTo(s2));
-                a[0] = new RecycleViewAdapter(words, recyclerView);
-                recyclerView.setAdapter(a[0]);
-            }
+        //when button is pressed
+        commitTextButton.setOnClickListener(v -> {
+            words.add(textToCommit.getText().toString());//add user's entry to list
+            words.sort((s1, s2) -> s1.compareTo(s2));//resort
+            a[0] = new RecycleViewAdapter(words, recyclerView);//and re-add so that recycler shows changes
+            recyclerView.setAdapter(a[0]);
         });
     }
 
